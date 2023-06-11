a85adb297769b97a471ed27b3285b69eb188b704
kurama0902 <creepysimbaplay@gmail.com>
2023-06-11 16:32:41 -0400

    Initial commit

---
+++ public/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{fontFamily: 'Arial', fontSize: '25px', color: 'teal'}}>Wine Project</h1>
+    </div>
+  );
+}
+
+export default App;
